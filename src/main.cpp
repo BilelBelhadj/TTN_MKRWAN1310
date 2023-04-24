074017354d1625c7205c762b695e4ba7ac61e555
BilelBelhadj <belhadj19bi@gmail.com>
2023-04-24 11:28:27 -0300

    final code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,17 @@
 /*
-  First Configuration
-  This sketch demonstrates the usage of MKR WAN 1300/1310 LoRa module.
-  This example code is in the public domain.
+Auteur      : Bilel Belhadj
+Titre       : The Things Network MKRWAN1310
+Description : Connect to the things network end device with the mkr wan 1310 and send the data collected with the two sensors
+Date        : 22-04-2023
+Version     : 0.0.1
+Source      : https://docs.arduino.cc/tutorials/mkr-wan-1310/the-things-network
+              https://www.hackster.io/ZoLuSs/first-step-with-mkr-wan-1310-and-the-things-networks-9f6065
 */
+
 #include <Arduino.h>
 #include <MKRWAN.h>
 
 LoRaModem modem;
-
-// Uncomment if using the Murata chip as a module
-// LoRaModem modem(Serial1);
-
-String appEui = "0000000000000000";
-String appKey = "C0608C705A53CFA8E338B09CDE5B844B";
 
 
 //constantes
@@ -20,47 +19,54 @@ const int mode = 1;
 const int connSensor = 6;
 const int lightSensor = A3;
 
+
 //variables
 int connected, err, light = 0, connectivity = 0;
+String appEui = "0000000000000000";                 //application EUI
+String appKey = "C0608C705A53CFA8E338B09CDE5B844B"; //Application key
+String data = "";
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(connSensor, INPUT);
 
-  while (!Serial);
+  Serial.begin(9600);         //demarrer le moniteur serie
+  pinMode(connSensor, INPUT); //pinmode broche connectivity
+
+  while (!Serial){}
+  
   Serial.println("Welcome to MKR WAN 1300/1310 first configuration sketch");
   Serial.println("Register to your favourite LoRa network and we are ready to go!");
-  // change this to your regional band (eg. US915, AS923, ...)
-  if (!modem.begin(EU868)) {
+  
+  //demarrage du modem
+  if (!modem.begin(US915)) {                    //regional canada band
     Serial.println("Failed to start module");
     while (1) {}
   };
-  Serial.print("Your module version is: ");
+
+  Serial.print("Your module version is: ");     //print modem version
   Serial.println(modem.version());
-  if (modem.version() != ARDUINO_FW_VERSION) {
+
+  if (modem.version() != ARDUINO_FW_VERSION) {  //verifier la version
     Serial.println("Please make sure that the latest modem firmware is installed.");
     Serial.println("To update the firmware upload the 'MKRWANFWUpdate_standalone.ino' sketch.");
   }
   Serial.print("Your device EUI is: ");
-  Serial.println(modem.deviceEUI());
+  Serial.println(modem.deviceEUI());      //afficher le 1310 EUI sur le moniteur serie
 
   appKey.trim();
   appEui.trim();
   Serial.print("trim done...!");
-  connected = modem.joinOTAA(appEui, appKey);
+  connected = modem.joinOTAA(appEui, appKey); //essayer de connecter
   Serial.println("first connection tried");
   
 
-  if (!connected) {
+  while (!connected) {    //en cas ou il ya pas de connection il essaye de se connecter encore jusqua ca se connecte
     Serial.println("Something went wrong; are you indoor? Move near a window and retry");
-    //while (1) {}
     connected = modem.joinOTAA(appEui, appKey);
     Serial.print(".");
     delay(50);
   }
 
-  if (connected) {
+  if (connected) {                   //affiche si c est connecter
     Serial.println("Connected...");
     delay(500);
   }
@@ -68,24 +74,32 @@ void setup() {
 
 void loop() {
 
+  //collecter les donnees du capteur
   light = analogRead(lightSensor);
   connectivity = digitalRead(connSensor);
 
-  modem.setPort(3);
-  modem.beginPacket();
-  modem.print(String(light));
-  modem.print(String(connectivity));
+  //construire la chaine
+  data = "{ light: " + String(light) + ", conductivity: " + String(connectivity) + "}";
+  Serial.println(data);
 
+  //send data
+  modem.setPort(3);     //port have to be the same as the ttn end_device
+  modem.beginPacket();  //start packet
+  modem.print(data);    //payload
   err = modem.endPacket(true);
+
+  //afficher message d'erreur
   if (err > 0) {
-    Serial.println("Message sent correctly!");
+    Serial.println("Message N1 sent correctly!");
   } else {
-    Serial.println("Error sending message :(");
+    Serial.println("Error sending message N1 :(");
   }
+
   
   while (modem.available()) {
     Serial.write(modem.read());
   }
-  modem.poll();
   
+  modem.poll();   //keep the connection alive
+  delay(12000);   //send data every 12 seconds
 }
